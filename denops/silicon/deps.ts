c85cfcb16ab84c0ae52c4ac7a9d43c0322dcb788
skanehira <skanehira@users.noreply.github.com>
2023-05-06 00:02:26 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/denops-silicon.vim/denops-silicon.vim/denops/silicon/deps.ts
    [1/8] Looking for releases: https://deno.land/x/denops_std@v4.1.6/mod.ts
    [1/8] Attempting update: https://deno.land/x/denops_std@v4.1.6/mod.ts -> v4.1.7
    [1/8] Update successful: https://deno.land/x/denops_std@v4.1.6/mod.ts -> v4.1.7
    [2/8] Looking for releases: https://deno.land/x/denops_std@v4.1.6/variable/mod.ts
    [2/8] Attempting update: https://deno.land/x/denops_std@v4.1.6/variable/mod.ts -> v4.1.7
    [2/8] Update successful: https://deno.land/x/denops_std@v4.1.6/variable/mod.ts -> v4.1.7
    [3/8] Looking for releases: https://deno.land/x/unknownutil@v2.1.0/mod.ts
    [3/8] Using latest: https://deno.land/x/unknownutil@v2.1.0/mod.ts
    [4/8] Looking for releases: https://deno.land/x/silicon@v0.0.5/mod.ts
    [4/8] Using latest: https://deno.land/x/silicon@v0.0.5/mod.ts
    [5/8] Looking for releases: https://deno.land/x/denops_std@v4.1.6/variable/mod.ts
    [5/8] Attempting update: https://deno.land/x/denops_std@v4.1.6/variable/mod.ts -> v4.1.7
    [5/8] Update successful: https://deno.land/x/denops_std@v4.1.6/variable/mod.ts -> v4.1.7
    [6/8] Looking for releases: https://deno.land/x/clippy@v0.2.1/mod.ts
    [6/8] Using latest: https://deno.land/x/clippy@v0.2.1/mod.ts
    [7/8] Looking for releases: https://deno.land/x/denops_std@v4.1.6/mapping/mod.ts
    [7/8] Attempting update: https://deno.land/x/denops_std@v4.1.6/mapping/mod.ts -> v4.1.7
    [7/8] Update successful: https://deno.land/x/denops_std@v4.1.6/mapping/mod.ts -> v4.1.7
    [8/8] Looking for releases: https://deno.land/std@0.186.0/streams/conversion.ts
    [8/8] Using latest: https://deno.land/std@0.186.0/streams/conversion.ts
    
    Already latest version:
    https://deno.land/x/unknownutil@v2.1.0/mod.ts == v2.1.0
    https://deno.land/x/silicon@v0.0.5/mod.ts == v0.0.5
    https://deno.land/x/clippy@v0.2.1/mod.ts == v0.2.1
    https://deno.land/std@0.186.0/streams/conversion.ts == 0.186.0
    
    Successfully updated:
    https://deno.land/x/denops_std@v4.1.6/mod.ts v4.1.6 -> v4.1.7
    https://deno.land/x/denops_std@v4.1.6/variable/mod.ts v4.1.6 -> v4.1.7
    https://deno.land/x/denops_std@v4.1.6/variable/mod.ts v4.1.6 -> v4.1.7
    https://deno.land/x/denops_std@v4.1.6/mapping/mod.ts v4.1.6 -> v4.1.7
    /home/runner/work/denops-silicon.vim/denops-silicon.vim/denops/silicon/deps_test.ts
    [1/2] Looking for releases: https://deno.land/std@0.186.0/testing/asserts.ts
    [1/2] Using latest: https://deno.land/std@0.186.0/testing/asserts.ts
    [2/2] Looking for releases: https://deno.land/x/denops_test@v1.1.0/mod.ts
    [2/2] Using latest: https://deno.land/x/denops_test@v1.1.0/mod.ts
    
    Already latest version:
    https://deno.land/std@0.186.0/testing/asserts.ts == 0.186.0
    https://deno.land/x/denops_test@v1.1.0/mod.ts == v1.1.0

--- denops/silicon/deps.ts
+++ denops/silicon/deps.ts
@@ -1,8 +1,8 @@
-export * from "https://deno.land/x/denops_std@v4.1.6/mod.ts";
-export * from "https://deno.land/x/denops_std@v4.1.6/variable/mod.ts";
+export * from "https://deno.land/x/denops_std@v4.1.7/mod.ts";
+export * from "https://deno.land/x/denops_std@v4.1.7/variable/mod.ts";
 export * from "https://deno.land/x/unknownutil@v2.1.0/mod.ts";
 export * as silicon from "https://deno.land/x/silicon@v0.0.5/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v4.1.6/variable/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v4.1.7/variable/mod.ts";
 export * as clippy from "https://deno.land/x/clippy@v0.2.1/mod.ts";
-export * as mapping from "https://deno.land/x/denops_std@v4.1.6/mapping/mod.ts";
+export * as mapping from "https://deno.land/x/denops_std@v4.1.7/mapping/mod.ts";
 export { readAll } from "https://deno.land/std@0.186.0/streams/conversion.ts";
