ca6cb855432d2550890e34d3c2ad92dc29b85043
skanehira <skanehira@users.noreply.github.com>
2023-09-20 00:02:29 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/denops-silicon.vim/denops-silicon.vim/denops/silicon/deps.ts
    [1/8] Looking for releases: https://deno.land/x/denops_std@v5.0.1/mod.ts
    [1/8] Using latest: https://deno.land/x/denops_std@v5.0.1/mod.ts
    [2/8] Looking for releases: https://deno.land/x/denops_std@v5.0.1/variable/mod.ts
    [2/8] Using latest: https://deno.land/x/denops_std@v5.0.1/variable/mod.ts
    [3/8] Looking for releases: https://deno.land/x/unknownutil@v3.6.0/mod.ts
    [3/8] Using latest: https://deno.land/x/unknownutil@v3.6.0/mod.ts
    [4/8] Looking for releases: https://deno.land/x/silicon@v0.0.6/mod.ts
    [4/8] Using latest: https://deno.land/x/silicon@v0.0.6/mod.ts
    [5/8] Looking for releases: https://deno.land/x/denops_std@v5.0.1/variable/mod.ts
    [5/8] Using latest: https://deno.land/x/denops_std@v5.0.1/variable/mod.ts
    [6/8] Looking for releases: https://deno.land/x/clippy@v0.2.2/mod.ts
    [6/8] Using latest: https://deno.land/x/clippy@v0.2.2/mod.ts
    [7/8] Looking for releases: https://deno.land/x/denops_std@v5.0.1/mapping/mod.ts
    [7/8] Using latest: https://deno.land/x/denops_std@v5.0.1/mapping/mod.ts
    [8/8] Looking for releases: https://deno.land/std@0.201.0/streams/read_all.ts
    [8/8] Attempting update: https://deno.land/std@0.201.0/streams/read_all.ts -> 0.202.0
    [8/8] Update successful: https://deno.land/std@0.201.0/streams/read_all.ts -> 0.202.0
    
    Already latest version:
    https://deno.land/x/denops_std@v5.0.1/mod.ts == v5.0.1
    https://deno.land/x/denops_std@v5.0.1/variable/mod.ts == v5.0.1
    https://deno.land/x/unknownutil@v3.6.0/mod.ts == v3.6.0
    https://deno.land/x/silicon@v0.0.6/mod.ts == v0.0.6
    https://deno.land/x/denops_std@v5.0.1/variable/mod.ts == v5.0.1
    https://deno.land/x/clippy@v0.2.2/mod.ts == v0.2.2
    https://deno.land/x/denops_std@v5.0.1/mapping/mod.ts == v5.0.1
    
    Successfully updated:
    https://deno.land/std@0.201.0/streams/read_all.ts 0.201.0 -> 0.202.0
    /home/runner/work/denops-silicon.vim/denops-silicon.vim/denops/silicon/deps_test.ts
    [1/2] Looking for releases: https://deno.land/std@0.201.0/testing/asserts.ts
    [1/2] Attempting update: https://deno.land/std@0.201.0/testing/asserts.ts -> 0.202.0
    [1/2] Update successful: https://deno.land/std@0.201.0/testing/asserts.ts -> 0.202.0
    [2/2] Looking for releases: https://deno.land/x/denops_test@v1.4.0/mod.ts
    [2/2] Using latest: https://deno.land/x/denops_test@v1.4.0/mod.ts
    
    Already latest version:
    https://deno.land/x/denops_test@v1.4.0/mod.ts == v1.4.0
    
    Successfully updated:
    https://deno.land/std@0.201.0/testing/asserts.ts 0.201.0 -> 0.202.0

--- denops/silicon/deps_test.ts
+++ denops/silicon/deps_test.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.201.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.202.0/testing/asserts.ts";
 export * from "https://deno.land/x/denops_test@v1.4.0/mod.ts";
