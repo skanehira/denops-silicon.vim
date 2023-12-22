f2e4477b6cb05d74c31dbc8b7006249beb562c01
skanehira <skanehira@users.noreply.github.com>
2023-12-22 00:02:44 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/denops-silicon.vim/denops-silicon.vim/denops/silicon/deps.ts
    [1/8] Looking for releases: https://deno.land/x/denops_std@v5.0.2/mod.ts
    [1/8] Attempting update: https://deno.land/x/denops_std@v5.0.2/mod.ts -> v5.2.0
    [1/8] Update successful: https://deno.land/x/denops_std@v5.0.2/mod.ts -> v5.2.0
    [2/8] Looking for releases: https://deno.land/x/unknownutil@v3.10.0/mod.ts
    [2/8] Attempting update: https://deno.land/x/unknownutil@v3.10.0/mod.ts -> v3.11.0
    [2/8] Update successful: https://deno.land/x/unknownutil@v3.10.0/mod.ts -> v3.11.0
    [3/8] Looking for releases: https://deno.land/x/denops_std@v5.0.2/batch/mod.ts
    [3/8] Attempting update: https://deno.land/x/denops_std@v5.0.2/batch/mod.ts -> v5.2.0
    [3/8] Update successful: https://deno.land/x/denops_std@v5.0.2/batch/mod.ts -> v5.2.0
    [4/8] Looking for releases: https://deno.land/x/denops_std@v5.0.2/function/mod.ts
    [4/8] Attempting update: https://deno.land/x/denops_std@v5.0.2/function/mod.ts -> v5.2.0
    [4/8] Update successful: https://deno.land/x/denops_std@v5.0.2/function/mod.ts -> v5.2.0
    [5/8] Looking for releases: https://deno.land/x/denops_std@v5.0.2/option/mod.ts
    [5/8] Attempting update: https://deno.land/x/denops_std@v5.0.2/option/mod.ts -> v5.2.0
    [5/8] Update successful: https://deno.land/x/denops_std@v5.0.2/option/mod.ts -> v5.2.0
    [6/8] Looking for releases: https://deno.land/x/denops_std@v5.0.2/variable/mod.ts
    [6/8] Attempting update: https://deno.land/x/denops_std@v5.0.2/variable/mod.ts -> v5.2.0
    [6/8] Update successful: https://deno.land/x/denops_std@v5.0.2/variable/mod.ts -> v5.2.0
    [7/8] Looking for releases: https://deno.land/x/clippy@v1.0.0/mod.ts
    [7/8] Using latest: https://deno.land/x/clippy@v1.0.0/mod.ts
    [8/8] Looking for releases: https://deno.land/x/silicon@v1.0.0/mod.ts
    [8/8] Using latest: https://deno.land/x/silicon@v1.0.0/mod.ts
    
    Already latest version:
    https://deno.land/x/clippy@v1.0.0/mod.ts == v1.0.0
    https://deno.land/x/silicon@v1.0.0/mod.ts == v1.0.0
    
    Successfully updated:
    https://deno.land/x/denops_std@v5.0.2/mod.ts v5.0.2 -> v5.2.0
    https://deno.land/x/unknownutil@v3.10.0/mod.ts v3.10.0 -> v3.11.0
    https://deno.land/x/denops_std@v5.0.2/batch/mod.ts v5.0.2 -> v5.2.0
    https://deno.land/x/denops_std@v5.0.2/function/mod.ts v5.0.2 -> v5.2.0
    https://deno.land/x/denops_std@v5.0.2/option/mod.ts v5.0.2 -> v5.2.0
    https://deno.land/x/denops_std@v5.0.2/variable/mod.ts v5.0.2 -> v5.2.0
    /home/runner/work/denops-silicon.vim/denops-silicon.vim/denops/silicon/deps_test.ts
    [1/2] Looking for releases: https://deno.land/std@0.207.0/assert/mod.ts
    [1/2] Attempting update: https://deno.land/std@0.207.0/assert/mod.ts -> 0.210.0
    [1/2] Update successful: https://deno.land/std@0.207.0/assert/mod.ts -> 0.210.0
    [2/2] Looking for releases: https://deno.land/x/denops_test@v1.4.0/mod.ts
    [2/2] Using latest: https://deno.land/x/denops_test@v1.4.0/mod.ts
    
    Already latest version:
    https://deno.land/x/denops_test@v1.4.0/mod.ts == v1.4.0
    
    Successfully updated:
    https://deno.land/std@0.207.0/assert/mod.ts 0.207.0 -> 0.210.0

--- denops/silicon/deps.ts
+++ denops/silicon/deps.ts
@@ -1,8 +1,8 @@
-export * from "https://deno.land/x/denops_std@v5.0.2/mod.ts";
-export * from "https://deno.land/x/unknownutil@v3.10.0/mod.ts";
-export * as batch from "https://deno.land/x/denops_std@v5.0.2/batch/mod.ts";
-export * as fn from "https://deno.land/x/denops_std@v5.0.2/function/mod.ts";
-export * as opts from "https://deno.land/x/denops_std@v5.0.2/option/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v5.0.2/variable/mod.ts";
+export * from "https://deno.land/x/denops_std@v5.2.0/mod.ts";
+export * from "https://deno.land/x/unknownutil@v3.11.0/mod.ts";
+export * as batch from "https://deno.land/x/denops_std@v5.2.0/batch/mod.ts";
+export * as fn from "https://deno.land/x/denops_std@v5.2.0/function/mod.ts";
+export * as opts from "https://deno.land/x/denops_std@v5.2.0/option/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v5.2.0/variable/mod.ts";
 export * as clippy from "https://deno.land/x/clippy@v1.0.0/mod.ts";
 export * as silicon from "https://deno.land/x/silicon@v1.0.0/mod.ts";
