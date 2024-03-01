53da2f23fde7e315a6b8725328adaa39cb07b5a1
skanehira <skanehira@users.noreply.github.com>
2024-03-01 00:02:49 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/denops-silicon.vim/denops-silicon.vim/denops/silicon/deps.ts
    [1/8] Looking for releases: https://deno.land/x/denops_std@v6.0.1/mod.ts
    [1/8] Attempting update: https://deno.land/x/denops_std@v6.0.1/mod.ts -> v6.2.0
    [1/8] Update successful: https://deno.land/x/denops_std@v6.0.1/mod.ts -> v6.2.0
    [2/8] Looking for releases: https://deno.land/x/unknownutil@v3.16.3/mod.ts
    [2/8] Using latest: https://deno.land/x/unknownutil@v3.16.3/mod.ts
    [3/8] Looking for releases: https://deno.land/x/denops_std@v6.0.1/batch/mod.ts
    [3/8] Attempting update: https://deno.land/x/denops_std@v6.0.1/batch/mod.ts -> v6.2.0
    [3/8] Update successful: https://deno.land/x/denops_std@v6.0.1/batch/mod.ts -> v6.2.0
    [4/8] Looking for releases: https://deno.land/x/denops_std@v6.0.1/function/mod.ts
    [4/8] Attempting update: https://deno.land/x/denops_std@v6.0.1/function/mod.ts -> v6.2.0
    [4/8] Update successful: https://deno.land/x/denops_std@v6.0.1/function/mod.ts -> v6.2.0
    [5/8] Looking for releases: https://deno.land/x/denops_std@v6.0.1/option/mod.ts
    [5/8] Attempting update: https://deno.land/x/denops_std@v6.0.1/option/mod.ts -> v6.2.0
    [5/8] Update successful: https://deno.land/x/denops_std@v6.0.1/option/mod.ts -> v6.2.0
    [6/8] Looking for releases: https://deno.land/x/denops_std@v6.0.1/variable/mod.ts
    [6/8] Attempting update: https://deno.land/x/denops_std@v6.0.1/variable/mod.ts -> v6.2.0
    [6/8] Update successful: https://deno.land/x/denops_std@v6.0.1/variable/mod.ts -> v6.2.0
    [7/8] Looking for releases: https://deno.land/x/clippy@v1.0.0/mod.ts
    [7/8] Using latest: https://deno.land/x/clippy@v1.0.0/mod.ts
    [8/8] Looking for releases: https://deno.land/x/silicon@v1.0.0/mod.ts
    [8/8] Using latest: https://deno.land/x/silicon@v1.0.0/mod.ts
    
    Already latest version:
    https://deno.land/x/unknownutil@v3.16.3/mod.ts == v3.16.3
    https://deno.land/x/clippy@v1.0.0/mod.ts == v1.0.0
    https://deno.land/x/silicon@v1.0.0/mod.ts == v1.0.0
    
    Successfully updated:
    https://deno.land/x/denops_std@v6.0.1/mod.ts v6.0.1 -> v6.2.0
    https://deno.land/x/denops_std@v6.0.1/batch/mod.ts v6.0.1 -> v6.2.0
    https://deno.land/x/denops_std@v6.0.1/function/mod.ts v6.0.1 -> v6.2.0
    https://deno.land/x/denops_std@v6.0.1/option/mod.ts v6.0.1 -> v6.2.0
    https://deno.land/x/denops_std@v6.0.1/variable/mod.ts v6.0.1 -> v6.2.0
    /home/runner/work/denops-silicon.vim/denops-silicon.vim/denops/silicon/deps_test.ts
    [1/2] Looking for releases: https://deno.land/std@0.216.0/assert/mod.ts
    [1/2] Attempting update: https://deno.land/std@0.216.0/assert/mod.ts -> 0.218.0
    [1/2] Update successful: https://deno.land/std@0.216.0/assert/mod.ts -> 0.218.0
    [2/2] Looking for releases: https://deno.land/x/denops_test@v1.6.2/mod.ts
    [2/2] Using latest: https://deno.land/x/denops_test@v1.6.2/mod.ts
    
    Already latest version:
    https://deno.land/x/denops_test@v1.6.2/mod.ts == v1.6.2
    
    Successfully updated:
    https://deno.land/std@0.216.0/assert/mod.ts 0.216.0 -> 0.218.0

--- denops/silicon/deps_test.ts
+++ denops/silicon/deps_test.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.216.0/assert/mod.ts";
+export * from "https://deno.land/std@0.218.0/assert/mod.ts";
 export * from "https://deno.land/x/denops_test@v1.6.2/mod.ts";
